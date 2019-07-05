61728c53908119bd82cef52e8be55bec724370bc
Chukwurado98 <chukwurado.umeaka98@gmail.com>
2019-07-05 13:39:43 -0400

    Add build control component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./BuildControls.module.css";
+
+function BuildControls(props) {
+  return <div />;
+}
+
+export default BuildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,16 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 function Burger({ ingredients }) {
-  const ingredientArray = Object.keys(ingredients).map(igKey => {
-    return [...Array(ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let ingredientArray = Object.keys(ingredients)
+    .map(igKey => {
+      return [...Array(ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .flat();
+  if (ingredientArray.length === 0) {
+    ingredientArray = <p>Please start adding ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
